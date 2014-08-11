30fc2e9f4bf06f2c7bf09e2460d166fec102edad
jt <jt@googleappengine.(none)>
2014-08-11 20:57:53 +0200

    table linking

--- test.py
+++ test.py
@@ -37,7 +37,12 @@ def process_csv(blob_info):
     # get file name
     csv_file_name =  blob_info.filename
     # get csv header
+    [prefix, postfix] = csv_file_name.split(".")
     header = reader.next()
+    temp_header = header
+    header = []
+    for col in temp_header:
+        header.append(prefix + "." + col)
         
 
     for row in reader:
@@ -45,20 +50,23 @@ def process_csv(blob_info):
         corresponding_facts = CorrespondingFacts()
         corresponding_facts.put()
         fact_keys = []
-        date_str = row[4]
+        if csv_file_name == "Sales.csv":
+            date_str = row[4]
         for col in row:
+            date =  datetime.datetime.strptime("1/1/14", '%m/%d/%y').date()
+            if csv_file_name == "Sales.csv":
+                 date = datetime.datetime.strptime(date_str, '%m/%d/%y').date()
             logging.info(col + "/n")
-            fact = Fact(value = col, 
+            fact = Fact(value = col,
                         name = header[col_counter],
                         corresponding_facts = corresponding_facts.key,
-                        date = datetime.datetime.strptime(date_str, '%m/%d/%y').date())
-
+                        date = date)
             fact.put()
             fact_keys.append(fact.key)
             col_counter += 1
-        corresponding_facts.facts = fact_keys
-        corresponding_facts.names = header
-        corresponding_facts.put()
+            corresponding_facts.facts = fact_keys
+            corresponding_facts.names = header
+            corresponding_facts.put()
  
 
 class Tests(webapp2.RequestHandler):
@@ -66,41 +74,45 @@ class Tests(webapp2.RequestHandler):
         logging.info("test")
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write("summary:")
-   
-        # summary of all facts
-        self.response.write("facts:")
-        facts = Fact.query()
-        for fact in facts:
-            self.response.write(str(fact) + "\n")
 
-
-        self.response.write("****************\n")        
-        self.response.write("*   TESTS      *\n")
-        self.response.write("****************\n") 
-
-        # Net Revenue
-        self.response.write("-------------\n")
-        self.response.write("Net Revenue 6/1/14:\n")
-        Fact.AddFactByMultiplication('Units', 'Royalty Price','Net Revenue')
-        date_060114 = datetime.datetime.strptime('6/1/14', '%m/%d/%y').date()
-        net_revenues = Fact.query(Fact.name=="Net Revenue", Fact.date == date_060114)
-        net_revenue_sum = 0
-        for fact in net_revenues:
-            self.response.write(str(fact) + "\n")
-            net_revenue_sum += int(fact.value)
-        self.response.write("Net Revenue sum:" + str(net_revenue_sum))    
-
-         # Filter
-        self.response.write("-------------\n")
-        self.response.write("Units filter by \
-                             Vendor Identifier:0268_20140114_SOFA_ENGLISHTEACHER):\n")
-        filtered_facts = Fact.FilterByDimLevel('Units', 
-                                               'Vendor Identifier',
-                                               '0268_20140114_SOFA_ENGLISHTEACHER')
+        filtered_facts = Fact.query(Fact.name == 'Sales.Vendor Identifier',
+                                    Fact.value ==  '0268_20140114_SOFA_ENGLISHTEACHER')
         for fact in filtered_facts:
             self.response.write(str(fact) + "\n")
 
+        # Test Corresponding Intra Fact
+        facts = Fact.query(Fact.name == "Sales.Units")
+        for fact in facts:
+            self.response.write(str(fact.getCorrespondingIntraFact("Sales.Royalty Price")) + "\n")
 
+        # Test Corresponding Inter Fact
+        fact_assault = Fact.query(Fact.name == "Sales.Vendor Identifier",
+                                  Fact.value == "0273_20140114_SOFA_ASSAULTONWALLSTREET")
+        for fact in fact_assault:
+            self.response.write(str(fact) + "\n")
+            corr_facts = fact.getCorrespondingInterFact("ComissionTax.Vendor Identifier")
+            for corr_fact in corr_facts:
+                self.response.write(str(corr_fact) + "\n")
+
+        # Test Chain
+        # Test Corresponding Inter Fact
+        self.response.write("CHAIN: \n")
+        fact_assaults = Fact.query(Fact.name == "Sales.Vendor Identifier",
+                                   Fact.value == "0273_20140114_SOFA_ASSAULTONWALLSTREET")
+        fact_assault = fact_assaults.get()
+        self.response.write(fact_assaults.get())
+
+        facts = fact_assault.getCorrespondingChainFact(["Sales.Country Code",
+                                                        "CountryRegion.Country Code",
+                                                        "CountryRegion.Region",
+                                                        "ComissionTax.Region",
+                                                        "ComissionTax.Tax Rate"])
+
+        
+        facts2 = fact_assault.getCorrespondingChainFact(["Sales.Units",
+                                                        "Sales.Vendor Identifier",
+                                                        "ComissionTax.Vendor Identifier",
+                                                         "ComissionTax.Tax Rate"])    
 
 
 class CorrespondingFacts(ndb.Model):
@@ -112,6 +124,14 @@ class CorrespondingFacts(ndb.Model):
         indices = [i for i, elem in enumerate(self.names) if name in elem]
         return self.facts[indices[0]].get()
 
+    def getCols(self,new_table):
+        cols = []
+        for name in self.names:
+            [table, col] = name.split(".")
+            col = new_table + "." + col
+            cols.append(col)
+        return cols
+
 
 class Fact(ndb.Model):
     value = ndb.StringProperty()
@@ -119,33 +139,37 @@ class Fact(ndb.Model):
     date = ndb.DateProperty()
     corresponding_facts = ndb.KeyProperty()
 
-    @staticmethod
-    def AddFactByMultiplication(first_fact_name, 
-                 second_fact_name,
-                 measure_name):
-         facts = Fact.query(Fact.name==first_fact_name)
-         for fact in facts:
-             corresponding_facts = fact.corresponding_facts.get()
-             second_fact = corresponding_facts.getFact(second_fact_name)
-             result = int(fact.value) * int (second_fact.value)
-             new_fact = Fact(value = str(result), 
-                        name = measure_name,
-                        corresponding_facts = fact.corresponding_facts,
-                        date = fact.date)
-             new_fact.put()
-             
-    @staticmethod
-    def FilterByDimLevel(fact_name, 
-                         dim_name,
-                         level_name):
-        facts = Fact.query(Fact.name == dim_name)
-        filtered_facts = []
-        for fact in facts:
-            if (fact.value == level_name):
-                corresponding_facts = fact.corresponding_facts.get()
-                target_fact = corresponding_facts.getFact(fact_name)
-                filtered_facts.append(target_fact)
-        return filtered_facts     
+    def getCorrespondingIntraFact(self, corresponding_fact_name):
+        corresponding_facts = self.corresponding_facts.get()
+        corresponding_fact = corresponding_facts.getFact(corresponding_fact_name)
+        return corresponding_fact
+       
+    def getCorrespondingInterFact(self, corresponding_fact_name):
+        return  Fact.query(Fact.name == corresponding_fact_name, 
+                           Fact.value == self.value) 
+        
+    def getCorrespondingChainFact(self, chain):
+        facts = []
+        facts.append(self)
+        last_fact_name = self.name;
+        for step in chain:
+            [last_table, last_col] = last_fact_name.split(".") 
+            [next_table, next_col] = step.split(".")
+            if last_table == next_table:
+                old_facts = facts
+                facts = []
+                for fact in old_facts:
+                    new_fact =  fact.getCorrespondingIntraFact(step)
+                    facts.append(new_fact)
+            else:
+                 old_facts = facts
+                 facts = []
+                 for fact in old_facts:
+                     new_facts =  fact.getCorrespondingInterFact(step)
+                     for new_fact in new_facts:
+                         facts.append(new_fact)
+            last_fact_name = step    
+        return facts    
 
     def __str__(self):
         return "FACT: name:" + self.name + \
